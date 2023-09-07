33274f6f9dc8d17cc3639043005fde974ded5e7a
ShreyaDeveloper2001 <134867803+ShreyaDeveloper2001@users.noreply.github.com>
2023-09-07 23:44:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
 //your code here
+<script>
+        document.addEventListener("DOMContentLoaded", function () {
+            const itemNameInput = document.getElementById("item-name-input");
+            const itemPriceInput = document.getElementById("item-price-input");
+            const addButton = document.getElementById("add");
+            const shoppingList = document.getElementById("shopping-list");
+            const totalElement = document.getElementById("total");
+
+            addButton.addEventListener("click", function () {
+                const itemName = itemNameInput.value.trim();
+                const itemPrice = parseFloat(itemPriceInput.value);
+if (!itemName || isNaN(itemPrice) || itemPrice <= 0) {
+                    alert("Invalid input. Please enter a valid item name and price.");
+                    return;
+                }
+
+                const newRow = document.createElement("tr");
+                const itemNameCell = document.createElement("td");
+                const itemPriceCell = document.createElement("td");
+
+                itemNameCell.textContent = itemName;
+                itemPriceCell.textContent = itemPrice.toFixed(2);
+
+                newRow.appendChild(itemNameCell);
+                newRow.appendChild(itemPriceCell);
+				shoppingList.insertBefore(newRow, totalElement.parentElement);
+
+                // Clear input fields
+                itemNameInput.value = "";
+                itemPriceInput.value = "";
+
+                // Update the total
+                const currentTotal = parseFloat(totalElement.textContent);
+                totalElement.textContent = (currentTotal + itemPrice).toFixed(2);
+            });
+        });
+    </script>
